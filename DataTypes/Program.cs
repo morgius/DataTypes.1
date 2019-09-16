dad90c1d54f8e2a2f567bbc58ec9721608643923
Paweł Podsiadło <p.podsiadlo@microsoft.wsei.edu.pl>
2019-09-16 18:06:23 +0200

    Add project files.

---
+++ DataTypes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = 0;
+            b = 10;
+            c = a / b;
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
